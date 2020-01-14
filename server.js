97cccc69ff8aead160745ad81e2ab68d79716fea
xiezihen <frank.xie@mail.utoronto.ca>
2020-01-14 00:14:04 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const express =  require('express');
+const app = express();
+const path= require('path');
+const port = process.env.PORT ||4200;
+
+app.use(express.static(__dirname + '/dist/eventApp'))
+app.listen(port)
+//PathLocationStrategy
+app.get('/*', (req,res) => {
+  res.sendFile(path.join(__dirname+'/dist/eventApp/index.html'));
+});
+console.log("Listening on port: "+port)
